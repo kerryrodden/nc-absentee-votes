25baf30729b337dd95cfcc98256b4f6dca1a72e1
Kerry Rodden <kerryrodden@users.noreply.github.com>
2020-09-29 20:25:23 -0700

    Create preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv("absentee_20201103.csv", encoding="ISO-8859-1")
+
+grouped = df.groupby(["county_desc", "race", "ethnicity", "ballot_request_party", "ballot_rtn_status"], as_index=False)["voter_reg_num"].count().rename(columns={"voter_reg_num": "count"})
+grouped.to_csv("absentee_20201103_aggregated.csv", index=False)
