ea1cd3a88deacfcc94d415a8ce98eab08c3c9e0c
Kerry Rodden <kerryrodden@users.noreply.github.com>
2020-10-19 18:13:28 -0700

    Update preprocess.py
    Include the ballot_req_type field in the grouping, so that it's possible to distinguish mailed ballots from early in-person votes.

--- preprocess.py
+++ preprocess.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 df = pd.read_csv("absentee_20201103.csv", encoding="ISO-8859-1")
 
-grouped = df.groupby(["county_desc", "race", "ethnicity", "ballot_request_party", "ballot_rtn_status"], as_index=False)["voter_reg_num"].count().rename(columns={"voter_reg_num": "count"})
+grouped = df.groupby(["county_desc", "race", "ethnicity", "ballot_req_type", "ballot_request_party", "ballot_rtn_status"], as_index=False)["voter_reg_num"].count().rename(columns={"voter_reg_num": "count"})
 grouped.to_csv("absentee_20201103_aggregated.csv", index=False)
